efb3623f6f9d4f5618b9d7becb91b5c274d2f5a9
gxxxh <guohao171@mails.ucas.ac.cn>
2022-04-19 23:11:09 +0800

    add and test gin get

--- gin/init.go
+++ gin/init.go
@@ -8,6 +8,8 @@ func initGin()*gin.Engine{
 	rg := r.Group("api/v1/uns_scheduler")
 	rg.GET("/hydra_result", GetHydraResult)
 	rg.GET("/edffast_result",GetEDFFastResult)
+	rg.GET("hydra_allocation", GetHydraAllocationPath)
+	rg.GET("edffast_allocation", GetEDFFastAllocation)
 	rg.POST("/submit_jobs", PostSubmitJobs)
 	//运行任务结果
 	//rg.POST()
